38e78b31a2ac4861a74e7d520d9b63624a40e5bf
fathoni <m.fathoni@mail.com>
2025-02-05 07:52:46 +0700

    initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+</script>
+
+<template>
+    <GuestLayout>
+        <h1>Judul</h1>
+        <p>Paragraf</p>
+    </GuestLayout>
+</template>
