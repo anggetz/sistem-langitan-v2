33a5282e4a280269e4f6085b34a30174a5a83ada
fathoni <m.fathoni@mail.com>
2025-02-13 03:22:39 +0700

    remove replace state

--- resources/js/LayoutComponents/Sidebar/SubmenuItem.vue
+++ resources/js/LayoutComponents/Sidebar/SubmenuItem.vue
@@ -11,7 +11,7 @@ defineProps({
 
 <template>
           <div class="menu-item" :class="{ 'active': $page.url === href }">
-           <Link class="menu-link border border-transparent items-center grow menu-item-active:bg-secondary-active dark:menu-item-active:bg-coal-300 dark:menu-item-active:border-gray-100 menu-item-active:rounded-lg hover:bg-secondary-active dark:hover:bg-coal-300 dark:hover:border-gray-100 hover:rounded-lg gap-[14px] ps-[10px] pe-[10px] py-[8px]" :href="href" replace tabindex="0">
+           <Link class="menu-link border border-transparent items-center grow menu-item-active:bg-secondary-active dark:menu-item-active:bg-coal-300 dark:menu-item-active:border-gray-100 menu-item-active:rounded-lg hover:bg-secondary-active dark:hover:bg-coal-300 dark:hover:border-gray-100 hover:rounded-lg gap-[14px] ps-[10px] pe-[10px] py-[8px]" :href="href" tabindex="0">
             <span class="menu-bullet flex w-[6px] -start-[3px] rtl:start-0 relative before:absolute before:top-0 before:size-[6px] before:rounded-full rtl:before:translate-x-1/2 before:-translate-y-1/2 menu-item-active:before:bg-primary menu-item-hover:before:bg-primary">
             </span>
             <span class="menu-title text-2sm font-normal text-gray-800 menu-item-active:text-primary menu-item-active:font-semibold menu-link-hover:!text-primary">
