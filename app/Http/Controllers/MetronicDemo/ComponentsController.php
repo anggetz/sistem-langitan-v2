792ddb87c3e3267d2ce49ff895595e886c3c0940
fathoni <m.fathoni@mail.com>
2025-02-17 03:33:18 +0700

    add metronic demo components

--- app/Http/Controllers/MetronicDemo/ComponentsController.php
+++ app/Http/Controllers/MetronicDemo/ComponentsController.php
@@ -13,8 +13,85 @@ class ComponentsController extends Controller
         Inertia::share('useDemoSidebar', true);
 
         switch ($component) {
+
             case 'accordion':
                 return Inertia::render('MetronicDemo/Components/Accordion');
+                break;
+
+            case 'avatar':
+                return Inertia::render('MetronicDemo/Components/Avatar');
+                break;
+
+            case 'badge':
+                return Inertia::render('MetronicDemo/Components/Badge');
+                break;
+
+            case 'button':
+                return Inertia::render('MetronicDemo/Components/Button');
+                break;
+
+            case 'button-group':
+                return Inertia::render('MetronicDemo/Components/ButtonGroup');
+                break;
+
+            case 'card':
+                return Inertia::render('MetronicDemo/Components/Card');
+                break;
+
+            case 'collapse':
+                return Inertia::render('MetronicDemo/Components/Collapse');
+                break;
+
+            case 'container':
+                return Inertia::render('MetronicDemo/Components/Container');
+                break;
+
+            case 'dismiss':
+                return Inertia::render('MetronicDemo/Components/Dismiss');
+                break;
+
+            case 'drawer':
+                return Inertia::render('MetronicDemo/Components/Drawer');
+                break;
+
+            case 'dropdown':
+                return Inertia::render('MetronicDemo/Components/Dropdown');
+                break;
+
+            case 'modal':
+                return Inertia::render('MetronicDemo/Components/Modal');
+                break;
+
+            case 'progress':
+                return Inertia::render('MetronicDemo/Components/Progress');
+                break;
+
+            case 'rating':
+                return Inertia::render('MetronicDemo/Components/Rating');
+                break;
+
+            case 'reparent':
+                return Inertia::render('MetronicDemo/Components/Reparent');
+                break;
+
+            case 'scrollable':
+                return Inertia::render('MetronicDemo/Components/Scrollable');
+                break;
+
+            case 'table':
+                return Inertia::render('MetronicDemo/Components/Table');
+                break;
+
+            case 'theme':
+                return Inertia::render('MetronicDemo/Components/Theme');
+                break;
+
+            case 'toggle':
+                return Inertia::render('MetronicDemo/Components/Toggle');
+                break;
+
+            case 'tooltip':
+                return Inertia::render('MetronicDemo/Components/Tooltip');
                 break;
 
             default:
---
+++ resources/js/Pages/MetronicDemo/Components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import ContainerFixed from '@/LayoutComponents/ContainerFixed.vue';
+</script>
+
+<template>
+  <AuthenticatedLayout>
+
+    <ContainerFixed>
+      <div class="flex flex-col gap-1.5 mb-5">
+        <h1 class="text-2xl font-bold text-gray-900">Card</h1>
+      </div>
+    </ContainerFixed>
+
+  </AuthenticatedLayout>
+</template>
