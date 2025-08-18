19053523fc983829546895af7f938162f2657f58
Nusantara Prasetya <pras.elang@gmail.com>
2025-08-18 12:47:02 +0700

    add default perguruan_tinggi to helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,11 @@
-<?php 
-function pt() {
-    if(app()->bound('pt')) {
+<?php
+
+use App\Models\PerguruanTinggi;
+
+function pt()
+{
+    if (app()->bound('pt')) {
         return app('pt');
     }
+    return PerguruanTinggi::find(env('APP_ID_PERGURUAN_TINGGI_DEFAULT'));
 }
