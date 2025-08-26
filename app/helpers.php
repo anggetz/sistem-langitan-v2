f8abbeb0eacf888a0f027e9a1b55643a58fe1a05
Nusantara Prasetya <pras.elang@gmail.com>
2025-08-26 16:49:31 +0700

    `Added caching for default Perguruan Tinggi instance in pt() helper function.`

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,19 @@
 <?php
 
+use App\Models\PerguruanTinggi;
+use Illuminate\Support\Facades\Cache;
+
 function pt()
 {
     if (app()->bound('pt')) {
         return app('pt');
     }
 
-    return App\Models\PerguruanTinggi::find(env('APP_ID_PERGURUAN_TINGGI_DEFAULT', 1));
+    $defaultPt = Cache::get("pt", null);
+    if ($defaultPt == null) {
+        $defaultPt = App\Models\PerguruanTinggi::find(env('APP_ID_PERGURUAN_TINGGI_DEFAULT'));
+        Cache::put("pt", $defaultPt, 600);
+    }
+
+    return $defaultPt;
 }
