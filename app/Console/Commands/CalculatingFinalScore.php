f954524494cbe6974d0710a02508c31f0f0b9529
Nusantara Prasetya <pras.elang@gmail.com>
2025-08-26 15:41:59 +0700

    fix penilaian huruf score

--- app/Console/Commands/CalculatingFinalScore.php
+++ app/Console/Commands/CalculatingFinalScore.php
@@ -143,9 +143,9 @@ class CalculatingFinalScore extends Command
                 'id_kelas_mk' => $idKelasMk,
                 'id_pengambilan_mk' => $finalScore['pengambilan_mk']->id_pengambilan_mk ?? null,
                 'fd_nilai_angka' => $finalScore['nilai'],
-                'fd_nilai_huruf' =>  $peraturanNilaiMinMax ? $peraturanNilaiMinMax->standardNilai->nm_standar_nilai : 'F',
+                'fd_nilai_huruf' =>  $peraturanNilaiMinMax ? $peraturanNilaiMinMax->standardNilai->nm_standar_nilai : '',
                 'nilai_angka' => $finalScore['nilai'],
-                'nilai_huruf' =>  $peraturanNilaiMinMax ? $peraturanNilaiMinMax->standardNilai->nm_standar_nilai : 'F',
+                'nilai_huruf' =>  $peraturanNilaiMinMax ? $peraturanNilaiMinMax->standardNilai->nm_standar_nilai : '',
             ]);
         }
 
