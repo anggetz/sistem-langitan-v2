46e9b580cbd088a141c67026bebf358229390721
N. Prasetya <pras.elang@gmail.com>
2025-12-04 22:51:45 +0700

    Refactor KegiatanController: Update update route to POST for form-data support and enhance validation logging

--- app/helpers.php
+++ app/helpers.php
@@ -3,17 +3,19 @@
 use App\Models\PerguruanTinggi;
 use Illuminate\Support\Facades\Cache;
 
-function pt()
-{
-    if (app()->bound('pt')) {
-        return app('pt');
-    }
+if (! function_exists('pt')) {
+    function pt()
+    {
+        if (app()->bound('pt')) {
+            return app('pt');
+        }
 
-    $defaultPt = Cache::get("pt", null);
-    if ($defaultPt == null) {
-        $defaultPt = App\Models\PerguruanTinggi::find(env('APP_ID_PERGURUAN_TINGGI_DEFAULT'));
-        Cache::put("pt", $defaultPt, 600);
-    }
+        $defaultPt = Cache::get("pt", null);
+        if ($defaultPt == null) {
+            $defaultPt = App\Models\PerguruanTinggi::find(env('APP_ID_PERGURUAN_TINGGI_DEFAULT'));
+            Cache::put("pt", $defaultPt, 600);
+        }
 
-    return $defaultPt;
+        return $defaultPt;
+    }
 }
